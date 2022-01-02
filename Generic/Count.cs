3367be6492bad49b86cb0e2c6c54815122836760
Andrey Salnikov <kvanta13@yandex.ru>
2022-01-03 00:15:02 +0300

    Добавьте файлы проекта.

---
+++ Generic/Count.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class Count<T>
+    {
+        T CountNumber { get; set; }
+        double Balance { get; set; }
+        string Name { get; set; }
+        public void WriteCountInfo()
+        {
+            Console.WriteLine("Введите номер счета");
+            CountNumber = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите баланс счета");
+            Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО держателя счета");
+            Name = Console.ReadLine();
+            Console.WriteLine("Для вывода результата нажмите любую клавищу");
+            Console.ReadKey();
+        }
+        public string ShowCountInfo()
+        {
+            return $"\"Информация по счету\"\n{CountNumber}\n{Balance}\n{Name}";
+        }
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Count<int> count = new Count<int>();
+            Count<string> count1 = new Count<string>();
+            count.WriteCountInfo();
+            Console.WriteLine(count.ShowCountInfo());
+            count1.WriteCountInfo();
+            Console.WriteLine(count1.ShowCountInfo());
+        }
+    }
+}
